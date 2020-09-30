c990a8abbb6d323c7230346b6bd4b10779f783f4
ARawles <adamrawles@hotmail.co.uk>
2020-09-30 12:08:51 +0100

    Updated functions to match new locations

--- main.R
+++ main.R
@@ -1,16 +1,16 @@
-create_pdf_version <- function(input_file = "docs/cv.Rmd") {
-  raw_rmd <- readLines(con = "docs/cv.Rmd")
+create_pdf_version <- function(input_file = "templates/cv.Rmd") {
+  raw_rmd <- readLines(con = input_file)
   pdf_rmd <- gsub('<a href = "/cv.pdf">PDF Version</a>', replacement = '', x = raw_rmd)
-  writeLines(text = pdf_rmd, con = "docs/cv_pdf.Rmd")
+  writeLines(text = pdf_rmd, con = "templates/cv_pdf.Rmd")
   closeAllConnections()
 }
 
 render_both <- function() {
-  rmarkdown::render("docs/cv.Rmd", output_dir = "host")
+  rmarkdown::render("templates/cv.Rmd", output_dir = "docs")
   create_pdf_version()
   
-  rmarkdown::render("docs/cv_pdf.Rmd")
-  pagedown::chrome_print("docs/cv_pdf.html", output = "host/cv.pdf")
+  rmarkdown::render("templates/cv_pdf.Rmd")
+  pagedown::chrome_print("templates/cv_pdf.html", output = "docs/cv.pdf")
 }
 
 render_both()
