86a18b0d8f8b82d2820a15a09b9f029a2ef81132
ARawles <adamrawles@hotmail.co.uk>
2020-09-30 11:24:19 +0100

    Init

---
+++ main.R
@@ -0,0 +1,14 @@
+create_pdf_version <- function(input_file = "docs/cv.Rmd") {
+  raw_rmd <- readLines(con = "docs/cv.Rmd")
+  pdf_rmd <- gsub('<a href = "/cv.pdf">PDF Version</a>', replacement = '', x = raw_rmd)
+  writeLines(text = pdf_rmd, con = "docs/cv_pdf.Rmd")
+  closeAllConnections()
+}
+
+render_both <- function() {
+  rmarkdown::render("docs/cv.Rmd")
+  create_pdf_version()
+  
+  rmarkdown::render("docs/cv_pdf.Rmd")
+  pagedown::chrome_print("docs/cv_pdf.html", output = "docs/cv.pdf")
+}
